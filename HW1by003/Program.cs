2861df0404185ae5f7cb366f1d6ba6748357b1f1
chayada0802 <chayada.0802@mail.kmutt.ac.th>
2021-08-31 10:35:27 +0700

    Add project files.

---
+++ HW1by003/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW1by003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
